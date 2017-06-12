40e8bd51b226fc8cfe8ec1b82ec73213e26dd914
yawnnn <alessandromy7476@gmail.com>
2017-06-12 10:28:39 +0200

    classe CheckFile

--- src/main/java/iubar/iva/export/CheckFile.java
+++ src/main/java/iubar/iva/export/CheckFile.java
@@ -6,13 +6,23 @@ import java.util.ArrayList;
 
 public class CheckFile {
 
+    private static final String FOLDER_PATH = System.getProperty("user.home") +
+            System.getProperty("file.separator") +
+            "Desktop" +
+            System.getProperty("file.separator") +
+            "Iva" +
+            System.getProperty("file.separator");
+
+    private static final String FILE_PATH = FOLDER_PATH + "iva_out.txt";
+
     private RandomAccessFile rw;
     private ArrayList<String> file = new ArrayList<String>();
+    private int counter = 0;
 
     CheckFile() {
         String line;
         try {
-            this.rw = new RandomAccessFile("/home/yawn/Desktop/iva_out.txt", "r");
+            this.rw = new RandomAccessFile(FILE_PATH, "r");
             while (null != (line = this.rw.readLine())) {
                 this.file.add(line);
             }
@@ -24,13 +34,14 @@ public class CheckFile {
         line = this.file.get(2);
         String[] lines = line.split("V[A-Z]{1}[0-9]{6}|V1{1}[0-9]{6}|V[A-Z]{1}[0-9]{5}[A-D]");
         for(String aLine : lines) {
+            counter++;
             System.out.println(aLine + " - " + aLine.length());
         }
+        System.out.println(counter);
     }
 
     public static void main(String[] args) {
-        //CheckFile a = new CheckFile();
-        System.out.println(System.getProperty("user.home") + System.getProperty("file.separator"));
+        CheckFile a = new CheckFile();
 
     }
 
